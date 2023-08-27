c7e51d5484258b6396a7e762fe7aaec3223091c0
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-08-27 15:32:20 -0300

    feat: comando persolnalizado

--- frontend/cypress/e2e/inicio.cy.js
+++ frontend/cypress/e2e/inicio.cy.js
@@ -1,11 +1,17 @@
 describe('template spec', () => {
-  it('Deve renderizar corretamente o texto da seção de vantagens ', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000/');
+  });
+
+  it('Deve renderizar corretamente o texto da seção de vantagens ', () => {
     cy.get('h2').contains('Vantagens do nosso banco:');
   });
 
+  it('Deve renderizar o h1 com texto correto', () => {
+    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+  });
+
   it('Deve ter 4 vantagens', () => {
-    cy.visit('http://localhost:3000/')
     cy.contains('Vantagens do nosso banco:');
     cy.get('[data-cy="vantagen"]').should('to.have.length', 4);
     cy.contains('Conta e cartão gratuitos');
